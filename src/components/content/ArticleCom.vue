937858262c8f93da0f248a39357309f399e94e8c
zhewei chen <1241770441@qq.com>
2023-03-08 08:57:49 +0800

    request封装

---
+++ src/components/content/ArticleCom.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ArticleCom",
+};
+</script>
+
+<style scoped></style>
--- src/views/Home/HomeCom.vue
+++ src/views/Home/HomeCom.vue
@@ -7,6 +7,7 @@
 
 <script>
 import NavBar from "@/components/Border/NavBar.vue";
+import request from "@/utils/request";
 
 export default {
   name: "HomeCom",
@@ -15,7 +16,31 @@ export default {
   data() {
     return {
       title: "头条",
+      //分页
+      page: 1,
+      //每页显示多少数据
+      limit: 10,
     };
+  },
+  methods: {
+    //登入后刷新首页，接受ArticleCom传过来的值
+    async initArticleComData() {
+      // 发起了GET请求，获取了文章的列表数据
+      const { data: res } = await request.get("/articles", {
+        // 1.请求数据
+        // 2.传递了两个参数，分别是页码和每页显示多少条数据
+        params: {
+          _page: this.page,
+          _limit: this.limit,
+        },
+      });
+      console.log(res);
+    },
+  },
+  // DOM创建时执行
+  created() {
+    //登入后刷新首页，接受ArticleCom传过来的值
+    this.initArticleComData();
   },
 };
 </script>
